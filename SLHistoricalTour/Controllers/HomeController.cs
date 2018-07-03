bf3b28c75f40739426e04d6259efb805cfbd8976
TheekshanaM <93.madushan@gmail.com>
2018-07-03 23:18:00 +0530

    finished part of project

--- SLHistoricalTour/Controllers/HomeController.cs
+++ SLHistoricalTour/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using PagedList;
+using PagedList.Mvc;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,8 +12,44 @@ namespace SLHistoricalTour.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string search, int? page)
         {
+            string[] Place = { "Abhayagiri dagaba", "Aukana Buddha Statue", "Gal Viharaya", "Isurumuniya Viharaya",
+                      "Jethawanaramaya Dagaba", "Kuttam Pokuna", "Lankatilaka Vihara", "Mirisawetiya Dagaba",
+                        "Ruwanweliseya","Samadhi Buddha Statue","Sandakada pahana", "Thuparamaya Dagaba" };
+
+
+            if(search != null)
+            {
+                int i = 0;
+                string sPattern = "^" + search;
+                foreach (string s in Place)
+                {
+                    if (System.Text.RegularExpressions.Regex.IsMatch(s, sPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                    {
+                        i++;
+                    }
+
+                }
+                string[] searched = new string[i];
+                i = 0;
+                foreach (string s in Place)
+                {
+                    if (System.Text.RegularExpressions.Regex.IsMatch(s, sPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                    {
+                        searched[i] = s;
+                        i++;
+                    }
+
+                }
+                return View(searched.ToList().ToPagedList(page ?? 1, 8));
+            }
+            return View(Place.ToList().ToPagedList(page ?? 1,8));
+        }
+
+        public ActionResult VirtualView(string name) {
+            name = Regex.Replace(name, " ", "");
+            ViewBag.Massege = ConfigurationManager.AppSettings["Host"].ToString() + "Image/Panorama/"+name+".jpg";
             return View();
         }
 
